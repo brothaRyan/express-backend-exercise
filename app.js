ea8c3b28cb9081b034bdf0064dcc14ed60b61497
Ryan <ryan.grlee@gmail.com>
2019-07-06 00:18:48 -0400

    HTTPS

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const models = require('./models');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const PassPhrase = process.env.REACT_APP_SSL_PASS_PHRASE;
+const KeyPath = process.env.REACT_APP_SSL_KEY_PATH;
+const CertPath = process.env.REACT_APP_SSL_CERT_PATH;
 
 function isEmpty(data) {
   return data === undefined || data == null || data.length === 0;
@@ -63,11 +73,18 @@ app.delete('/todos/:id', (req, res) => {
   })
 });
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+// app.listen(3000, () => {
+//   console.log('Example app listening on port 3000!');
 
-  require('./models').sequelize.sync({force: false})
-    .then(() => {
-      console.log('Databases sync');
-    });
-});
+//   require('./models').sequelize.sync({force: false})
+//     .then(() => {
+//       console.log('Databases sync');
+//     });
+// });
+
+https.createServer({
+  key: fs.readFileSync(KeyPath),
+  cert: fs.readFileSync(CertPath),
+  passphrase: PassPhrase
+}, app)
+.listen(3000);
